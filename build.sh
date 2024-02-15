fe6dc1fb43209b35369636301237dc9b6a582d3c
helloworld <cxchunxiao@gmail.com>
2024-02-16 00:48:00 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+npm run build || exit
+export tag=`date "+%Y%m%d%H%M%S"` || exit
+docker build -f Dockerfile -t cc-frontend:${tag} . || exit
+docker tag cc-frontend:${tag} registry.cn-hangzhou.aliyuncs.com/projectcc/cc-frontend:${tag} || exit
+docker push registry.cn-hangzhou.aliyuncs.com/projectcc/cc-frontend:${tag} || exit
